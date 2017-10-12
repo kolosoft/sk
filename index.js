7ed7dd4911f42b6c7ab2a023e304b8534c9b7181
Andrey A. Kolosov <a.kolosov@pulkovo-service.ru>
2017-10-12 11:23:45 +0300

    conflict resolved

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ function xyz(a) {
     alert(a);
 }
 
-<<<<<<< HEAD
 function y3(y) {
    return y * y * y;
-=======
+}
+
 function x2(x) {
     return x*x;
->>>>>>> 6b903830c92d5cf85d189280b638b93294885444
 }
